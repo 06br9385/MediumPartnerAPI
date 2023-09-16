470a3a036b50b960f92c2a329dface871fd1acce
makbas <makbas@SNET-MAKBAS>
2023-09-16 16:00:05 +0300

    ok

---
+++ MediumPartnerAPI/Controllers/MediumPartnerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MediumPartnerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MediumPartnerController : ControllerBase
+    {
+        [HttpGet]
+        public  async Task<IActionResult> GetAllPartners()
+        {
+            var mediumPartners = new List<Models.MediumPartner>
+            {
+                new Models.MediumPartner
+                {
+                    Id = 1,
+                    Name = "TestUser",
+                    FirstName = "Test",
+                    LastName = "User",
+                    Place = "New York City"
+                }
+            };
+
+            return Ok(mediumPartners); 
+        }
+    }
+}
